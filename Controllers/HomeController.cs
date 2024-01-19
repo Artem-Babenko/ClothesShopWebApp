ee557bbf1decb6403b587fb78d206f382ba85e2e
Babenko Artem <babenkoartem505@gmail.com>
2024-01-19 22:09:20 +0200

    Added main page request handler

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,5 +4,11 @@ namespace WearShopWebApp.Controllers;
 
 public class HomeController : Controller
 {
-
+    // Отримання головної сторінки.
+    [HttpGet]
+    [Route("/")]
+    public IActionResult HomePage()
+    {
+        return View("/Views/Home/Index.cshtml");
+    }
 }
