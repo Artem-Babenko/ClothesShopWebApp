e75790f2dea3512082ddda09eb6e823611367ad7
Babenko Artem <babenkoartem505@gmail.com>
2024-01-19 22:04:55 +0200

    Added a model that represents a unit of clothing color

---
+++ Models/Color.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WearShopWebApp.Models;
+
+// Представляє інформацію про колір одягу.
+public class Color
+{
+    [Key] public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Hex { get; set; }
+
+    public Clothes? Clothes { get; set; }
+
+    public int? ClothesId { get; set; }
+}
