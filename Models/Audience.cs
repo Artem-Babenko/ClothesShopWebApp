2f7c8cbe2f105180ed65a6853c1adcb5609d1ca4
Babenko Artem <babenkoartem505@gmail.com>
2024-01-19 22:05:34 +0200

    Fix property name

--- Models/Audience.cs
+++ Models/Audience.cs
@@ -6,7 +6,7 @@ public class Audience
 {
     [Key] public int Id { get; set; }
     
-    public string? Name { get; set; }
+    public string? Type { get; set; }
 
     public List<Category> Categories { get; set; } = new List<Category>();
 }
